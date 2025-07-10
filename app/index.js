79ab5e20b056d86517a263024a654b2a3a0521e1
x-bl <pengagumalam2130@gmail.com>
2025-07-11 02:48:27 +0700

    Create index.js

---
+++ app/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const client = require('prom-client');
+
+const app = express();
+const port = 3000;
+
+// Prometheus metrics
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+const httpRequestDurationMicroseconds = new client.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [0.1, 0.5, 1, 1.5, 2, 5]
+});
+
+app.use((req, res, next) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  res.on('finish', () => {
+    end({ method: req.method, route: req.path, code: res.statusCode });
+  });
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js App with Prometheus!');
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
